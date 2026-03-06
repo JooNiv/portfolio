57817a1e92566a265f329e52d8259bec6a64cf20
Joonas Nivala <joonas.nivala@gmail.com>
2026-03-06 23:15:40 +0200

    update site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import solidJs from '@astrojs/solid-js';
 // https://astro.build/config
 export default defineConfig({
   integrations: [solidJs()],
-  site: 'https://jooniv.github.io',
-  base: '/portfolio',
+  site: 'https://jooniv.fi',
   
 });
