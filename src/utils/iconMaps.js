1e16d9b689966403aaa0ebefa2bfb897fbc8a0aa
Joonas Nivala <joonas.nivala@gmail.com>
2026-03-06 23:37:37 +0200

    new project

--- src/utils/iconMaps.js
+++ src/utils/iconMaps.js
@@ -4,11 +4,13 @@ import { MdFillContent_cut } from 'solid-icons/md'
 import { CgWebsite } from 'solid-icons/cg'
 import { VsSearchFuzzy } from 'solid-icons/vs'
 import { FaBrandsReact } from 'solid-icons/fa'
+import { BiRegularServer } from 'solid-icons/bi'
 import { TbFillMoodHappy } from 'solid-icons/tb'
 
 export const projectIconMap = {
     qcut: MdFillContent_cut,
     portfolio: CgWebsite,
+    portfoliobackend: BiRegularServer,
     qcutfind: VsSearchFuzzy,
     fiqciweb: FaBrandsReact,
     moodpixels: TbFillMoodHappy,
