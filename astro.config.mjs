539705acc3b13516db961a7c984b2cffd4f33111
Joonas Nivala <joonas.nivala@gmail.com>
2026-03-12 12:09:42 +0200

    fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import solidJs from '@astrojs/solid-js';
 
-import node from '@astrojs/node';
-
-// https://astro.build/config
 export default defineConfig({
   integrations: [solidJs()],
   site: 'https://jooniv.fi',
-
-  output: "server",
-  adapter: node({
-    mode: "standalone"
-  }),
-  vite: {
-    ssr: {
-      noExternal: true
-    }
-  }
 });
