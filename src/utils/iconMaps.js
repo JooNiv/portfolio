11c178adfbece655ac22e493f346047d62126216
Joonas Nivala <joonas.nivala@gmail.com>
2026-03-12 12:24:48 +0200

    edit projects

--- src/utils/iconMaps.js
+++ src/utils/iconMaps.js
@@ -6,6 +6,8 @@ import { VsSearchFuzzy } from 'solid-icons/vs'
 import { FaBrandsReact } from 'solid-icons/fa'
 import { BiRegularServer } from 'solid-icons/bi'
 import { TbFillMoodHappy } from 'solid-icons/tb'
+import { FaSolidGamepad } from 'solid-icons/fa'
+import { AiOutlineCloudServer } from 'solid-icons/ai'
 
 export const projectIconMap = {
     qcut: MdFillContent_cut,
@@ -13,6 +15,8 @@ export const projectIconMap = {
     portfoliobackend: BiRegularServer,
     qcutfind: VsSearchFuzzy,
     fiqciweb: FaBrandsReact,
+    entanglementgamefrontend: FaSolidGamepad,
+    entanglementgamebackend: AiOutlineCloudServer,
     moodpixels: TbFillMoodHappy,
 }
 
